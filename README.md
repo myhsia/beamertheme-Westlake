48d178d6801ab5e7ee75566e05ba083107a5ae67
myhsia <myhsia@outlook.com>
2026-03-02 16:53:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+[![CTAN Version](https://img.shields.io/ctan/v/beamertheme-Westlake)](https://ctan.org/pkg/beamertheme-Westlake)
+[![GitHub Release](https://img.shields.io/github/v/release/myhsia/beamertheme-Westlake)](https://github.com/myhsia/beamertheme-Westlake/releases/latest)
+[![GitHub Last Commit](https://img.shields.io/github/last-commit/myhsia/beamertheme-Westlake)](https://github.com/myhsia/beamertheme-Westlake/commits)
+[![Actions Status](https://github.com/myhsia/beamertheme-Westlake/actions/workflows/main.yaml/badge.svg?branch=main)](https://github.com/myhsia/beamertheme-Westlake/actions)
+[![GitHub Repo stars](https://img.shields.io/github/stars/myhsia/beamertheme-Westlake)](https://github.com/myhsia/beamertheme-Westlake)
+
+The `Westlake` Beamer Theme
+===========================
+
+Overview
+--------
+
+To load this theme under the `beamer` class, write the lines
+
+    \usetheme[logo = <file>, badge = <file>, mainbg = <file>, basebg = <file>]{Westlake}
+
+to set the theme color and graphics.
+
+See `beamertheme-Westlake.pdf` for more. Happy TeXing!
+
+Issues
+------
+
+The issue tracker for `beamertheme-Westlake` is currently located
+[on GitHub](https://github.com/myhsia/pbeamertheme-Westlake).
+
+Build status
+------------
+
+This project uses [GitHub Actions](https://github.com/features/actions)
+as a hosted continuous integration service. For each commit, the build status
+is tested using the current release of TeX Live.
+
+_Current build status:_ ![build status](https://github.com/myhsia/beamertheme-Westlake/actions/workflows/main.yaml/badge.svg?branch=main)
+
+Copyright and License
+---------------------
+
+Copyright (C) 2026 by Mingyu Xia \<myhsia@outlook.com\>
+
+This work may be distributed and/or modified under the conditions
+of the LaTeX Project Public License (LPPL), either version 1.3c of
+this license or (at your option) any later version.
+The latest version of this license is in
+
+    http://www.latex-project.org/lppl.txt
+
+and version 1.3c or later is part of all distributions of LaTeX
+version 2008 or later.
+
+This work has the LPPL maintenance status `maintained'.
+
+The Current Maintainer of this work is **Mingyu Xia**.
---
+++ build.lua
@@ -0,0 +1,94 @@
+--[==========================================[--
+        L3BUILD FILE FOR BEAMER-WESTLAKE
+      Once Pushed With This File Modified
+        A New Release Will Be Published
+--]==========================================]--
+
+--[==========================================[--
+               Basic Information
+             Do Check Before Push
+--]==========================================]--
+
+module              = "beamertheme-Westlake"
+version             = "v0.1A"
+date                = "2026-03-01"
+maintainer          = "Mingyu Xia"
+uploader            = "Mingyu Xia"
+maintainid          = "myhsia"
+email               = "myhsia@outlook.com"
+repository          = "https://github.com/" .. maintainid .. "/" .. module
+summary             = ""
+description         = ""
+
+--[==========================================[--
+          Build, Pack, Tag, and Upload
+         Do not Modify Unless Necessary
+--]==========================================]--
+
+ctanzip             = module
+cleanfiles          = {"*.log", "*.pdf", "*.zip", "*.curlopt"}
+docsuppdirs         = {"media"}
+excludefiles        = {"*~"}
+typesetdemofiles    = {module .. "-demo.tex"}
+textfiles           = {"*.md", "LICENSE", "*.lua"}
+typesetexe          = "latexmk -pdflatex"
+typesetruns         = 1
+uploadconfig  = {
+  announcement_file = "announcement.md",
+  pkg               = module,
+  version           = version .. " " .. date,
+  author            = maintainer,
+  uploader          = uploader,
+  email             = email,
+  summary           = summary,
+  description       = description,
+  license           = "lppl1.3c",  
+  ctanPath          = "/macros/latex/contrib/" .. module,
+  home              = "https://github.com/" .. maintainid,
+  bugtracker        = repository .. "/issues",
+  support           = repository .. "/issues",
+  repository        = repository,
+  development       = "https://github.com/" .. maintainid,
+  update            = true
+}
+function update_tag(file, content, tagname, tagdate)
+  tagname = version
+  tagdate = date
+  if string.match(file, "%.dtx$") then
+    content = string.gsub(content,
+      "\\def \\westlake@date    %{[^}]+%}",
+      "\\def \\westlake@date    {" .. tagdate .. "}")
+    content = string.gsub(content,
+      "\\def \\westlake@version %{[^}]+%}",
+      "\\def \\westlake@version {" .. tagname .. "}")
+    content = string.gsub(content,
+      "\\date{Released %d+%-%d+%-%d+\\quad \\texttt{v([%d%.A-Z]+)}}",
+      "\\date{Released " .. tagdate .. "\\quad \\texttt{" .. tagname .. "}}")
+  end
+  return content
+end
+
+--[== "Hacks" to `l3build` | Do not Modify ==]--
+
+function docinit_hook()
+  cp(ctanreadme, unpackdir, currentdir)
+  for _,glob in pairs(docsuppdirs) do
+    run(currentdir, "cp -r " .. glob .. " " .. typesetdir)
+  end
+  return 0
+end
+function tex(file,dir,cmd)
+  dir = dir or "."
+  cmd = cmd or typesetexe
+  if os.getenv("WINDIR") ~= nil or os.getenv("COMSPEC") ~= nil then
+    upretex_aux = "-usepretex=\"" .. typesetcmds .. "\""
+    makeidx_aux = "-e \"$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\""
+    sandbox_aux = "set \"TEXINPUTS=../unpacked;%TEXINPUTS%;\" &&"
+  else
+    upretex_aux = "-usepretex=\'" .. typesetcmds .. "\'"
+    makeidx_aux = "-e \'$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\'"
+    sandbox_aux = "TEXINPUTS=\"../unpacked:$(kpsewhich -var-value=TEXINPUTS):\""
+  end
+  return run(dir, sandbox_aux .. " " .. cmd         .. " " ..
+                  upretex_aux .. " " .. makeidx_aux .. " " .. file)
+end
